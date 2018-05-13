b283d774cbed8285f5cc24b8ed281bd031fffee8
lzz7626 <lzz@7626>
2018-05-13 11:40:06 +0800

    初始化项目

---
+++ springboot-learning-example-master/chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
@@ -0,0 +1,32 @@
+package demo.springboot.web;
+
+import demo.springboot.config.BookComponent;
+import demo.springboot.config.BookProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot Hello案例
+ *
+ * Created by bysocket on 26/09/2017.
+ */
+@RestController
+public class HelloBookController {
+
+    @Autowired
+    BookProperties bookProperties;
+    @Autowired
+    BookComponent bookComponent;
+
+    @GetMapping("/book/hello")
+    public String sayHello() {
+        return "Hello， " + bookProperties.getWriter() + " is writing "
+                + bookProperties.getName() + " ！";
+    }
+
+    @GetMapping("/book/say")
+    public String helloSay(){
+        return "Say, " + bookComponent.getWriter() + " is writing " + bookComponent.getName() + " .";
+    }
+}
